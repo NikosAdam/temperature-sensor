931c263ea20c85cbe1aacd4558e68ce6388d23c2
NikosAdam <55497532+NikosAdam@users.noreply.github.com>
2019-09-23 17:45:52 +0300

    update sensor.c

--- sensor.c
+++ sensor.c
@@ -1,40 +1,3 @@
-/********************************************************************
- FileName:     	<Application Name Here>.c
- Dependencies:	See INCLUDES section
- Processor:		PIC16F690 Microcontrollers
-
- Compiler:		HI-TECH C PRO for the PIC10/12/16 MCU Family (Lite)
- Company:		Microchip Technology, Inc.
- Engineer:		<Your Name Here>
-
- Software License Agreement:
-
- The software supplied herewith by Microchip Technology Incorporated
- (the "Company") for its PIC? Microcontroller is intended and
- supplied to you, the Company's customer, for use solely and
- exclusively on Microchip PIC Microcontroller products. The
- software is owned by the Company and/or its supplier, and is
- protected under applicable copyright laws. All rights are reserved.
- Any use in violation of the foregoing restrictions may subject the
- user to criminal sanctions under applicable laws, as well as to
- civil liability for the breach of the terms and conditions of this
- license.
-
- THIS SOFTWARE IS PROVIDED IN AN "AS IS" CONDITION. NO WARRANTIES,
- WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
- TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
- PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
- IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
- CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
-
-********************************************************************
- File Description:
-
- Change History:
-  Rev   Date         Description
-
-
-********************************************************************/
 #include	<pic.h>
 
 
@@ -178,8 +141,7 @@ void Get_Inputs(void)
 ----------------------------------------------------------*/
 void Decide(void)
 {
-	//Assign the upper 4 bits of ADRESH to the lower 4 bits
-	//of LED_Output
+	
 	LED_Output = ADRESH >> 4; 	//Shifts the bits in ADRESL 4 bits
 								//to the right
 }
@@ -197,16 +159,3 @@ void Do_Outputs(void)
 	//LEDs
 	PORTC = LED_Output;
 }
-
-/*----------------------------------------------------------
-	Subroutine: Timing
-	Parameters: none
-	Returns:	nothing
-	Synopsys:	Controls the how fast code in the main() is executed
-----------------------------------------------------------*/
-void Timing(void)
-
-{
-	//ADD TIMING CODE HERE
-}
-
